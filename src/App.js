3b077db68ca36b26bd3d0fee9b2f26ae4577efd3
Omprakash <omprakashpandit524@gmail.com>
2023-01-28 00:12:42 +0530

    react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import ExpenseItem from './components/Expenseitem';
 function App() {
   return (
     <div>
+      <h1>Learning React </h1>
       <h2>Let's get started! with react</h2>
+      
       <ExpenseItem></ExpenseItem>
     </div>
   );
